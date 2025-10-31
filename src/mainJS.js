0927b24b20aa5fb06f6f1e11a018505e8abf4a48
Ari <arlenehdez0225@gmail.com>
2025-10-30 21:22:25 -0400

    Sidebar
    Made a collapsible side-bar, still needs works.

--- src/mainJS.js
+++ src/mainJS.js
@@ -1,4 +1,12 @@
 
+function openChat(){
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+function closeChat(){
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+}
 //this is just to create the skeleton and upload to github, can be removed
 const buttonTest = document.getElementById("buttontTest");
 let checker = true;
